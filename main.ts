3a92dbfe28d9aa26ffa892fe7faab8dad245fda1
cookie-y <2244242892@qq.com>
2024-01-15 09:21:05 +0800

    添加UViewPlus依赖

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 import App from './App.vue';
 import 'virtual:uno.css';
+import uviewPlus from '@/uni_modules/uview-plus';
 
 import { createSSRApp } from 'vue';
 export function createApp(): any {
   const app = createSSRApp(App);
+  app.use(uviewPlus);
   return {
     app,
   };
--- pages/auth/SignIn.vue
+++ pages/auth/SignIn.vue
@@ -1,5 +1,12 @@
 <template>
-  <view class="content"> 登录 </view>
+  <view class="content">
+    <u-image
+      :show-loading="true"
+      src="/static/image/system/images/cooking.png"
+      width="750rpx"
+      height="780rpx"
+    ></u-image>
+  </view>
 </template>
 
 <script lang="ts"></script>
