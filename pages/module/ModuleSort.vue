61d587f5c0f95808b690ec6e545a7d0a87765d4e
cookie-y <2244242892@qq.com>
2024-01-18 09:42:26 +0800

    分类管理静态页面

---
+++ pages/module/ModuleSort.vue
@@ -0,0 +1,32 @@
+<template>
+  <view>
+    <Navigation title="分类" />
+    <view class="flex-inline ml-20 mt-10">
+      <u-button size="small" icon="plus" color="#232946" iconColor="#fff">添加</u-button>
+    </view>
+    <view class="mt-10">
+      <SortItem v-for="item in sortList" :key="item.id" :title="item.title" />
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import SortItem from './components/SortItem.vue';
+
+const sortList = [
+  {
+    id: 1,
+    title: '主食',
+  },
+  {
+    id: 2,
+    title: '饮料',
+  },
+  {
+    id: 3,
+    title: '甜品',
+  },
+];
+</script>
+
+<style></style>
---
+++ pages/module/components/SortItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="flex items-center justify-between bg-fff pr-20 pl-20 pt-10 pb-10" :class="$style['border']">
+    <text class="fs-16">{{ title }}</text>
+    <view class="flex-inline">
+      <u-icon name="/static/image/system/icons/edit.png" size="24" class="mr-5" />
+      <u-icon name="/static/image/system/icons/delete.png" size="24" />
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+interface TProps {
+  title: string;
+}
+withDefaults(defineProps<TProps>(), {});
+</script>
+
+<style lang="scss" module>
+.border {
+  border-bottom: 1px solid $uni-border-color;
+}
+</style>
