03fa5e3abe9a80b7b2788b5e8e678aa06a7c0165
cookie-y <2244242892@qq.com>
2024-01-18 10:22:07 +0800

    联系我们静态页面

---
+++ pages/me/ContactUs.vue
@@ -0,0 +1,67 @@
+<template>
+  <view class="relative">
+    <Navigation title="联系我们" />
+    <view class="mt-20">
+      <CommentCard
+        v-for="item in commentList"
+        :key="item.id"
+        :username="item.username"
+        :title="item.title"
+        :avatar="item.image"
+        :content="item.content"
+        :time="item.time"
+      />
+    </view>
+
+    <u-button color="#232946" :class="$style['leave-comment']">我要留言</u-button>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import CommentCard from './components/CommentCard.vue';
+
+const commentList = ref([
+  {
+    id: 1,
+    username: '曲奇',
+    image: '/static/image/user/recipes/bread.png',
+    title: '标题标题',
+    content: '内容内容内内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容容内容',
+    time: '2024-1-18 09:55',
+  },
+  {
+    id: 2,
+    username: '曲奇',
+    image: '/static/image/user/recipes/bread.png',
+    title: '标题标题',
+    content: '内容内容内容内内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容容',
+    time: '2024-1-18 09:55',
+  },
+  {
+    id: 3,
+    username: '曲奇',
+    image: '/static/image/user/recipes/bread.png',
+    title: '标题标题',
+    content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+    time: '2024-1-18 09:55',
+  },
+  {
+    id: 3,
+    username: '曲奇',
+    image: '/static/image/user/recipes/bread.png',
+    title: '标题标题',
+    content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+    time: '2024-1-18 09:55',
+  },
+]);
+</script>
+
+<style module lang="scss">
+.leave-comment {
+  position: fixed;
+  bottom: 70px;
+  left: calc(50% - 60px);
+  width: 120px;
+}
+</style>
---
+++ pages/me/components/CommentCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="flex p-20 bg-fff rounded-6 mb-15">
+    <u-image :src="avatar" width="80rpx" height="80rpx" />
+    <view class="flex flex-col ml-10">
+      <text class="fs-14">{{ username }}</text>
+      <text class="mt-25 fs-16">{{ title }}</text>
+      <u-text :text="content" lines="5" size="14" color="#333" margin="10rpx 0 0" />
+      <view class="mt-5">
+        <text class="fs-12 c-999">{{ time }}</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+interface TProps {
+  avatar: string;
+  username: string;
+  title: string;
+  content: string;
+  time: string;
+}
+withDefaults(defineProps<TProps>(), {});
+</script>
+
+<style></style>
